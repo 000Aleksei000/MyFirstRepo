1e5447b5db0d5ac850d6ea517f26bb2b73ef1bda
AlekseySergeev <Lexxx_95@bk.ru>
2021-02-15 17:21:45 +0300

    Generic's generator

---
+++ src/main/java/JavaCollection/task35/task3512/Generator.java
@@ -0,0 +1,13 @@
+package JavaCollection.task35.task3512;
+
+public class Generator<T> {
+
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass){
+        this.aClass = aClass;
+    }
+    T newInstance() throws IllegalAccessException, InstantiationException {
+        return (T)aClass.newInstance();
+    }
+}
