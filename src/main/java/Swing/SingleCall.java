8bd701fbce7aa9b174c6eb16a6a515090ad89a91
AlekseySergeev <Lexxx_95@bk.ru>
2021-02-24 19:39:03 +0300

    EventHandler

---
+++ src/main/java/Swing/SingleCall.java
@@ -0,0 +1,34 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+
+public class SingleCall {
+    public static void main(String[] args) {
+        JFrame jFrame = getFrame();
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+        JButton jButton = new JButton("submit");
+        jPanel.add(jButton);
+        /*jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jFrame.setTitle(((JButton)e.getSource()).getText());
+            }
+        });*/
+        jButton.addActionListener(EventHandler.create(ActionListener.class, jFrame, "title", "source.text"));
+    }
+
+    static JFrame getFrame(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 300, dimension.height / 2 - 200, 600, 400);
+        return jFrame;
+    }
+}
