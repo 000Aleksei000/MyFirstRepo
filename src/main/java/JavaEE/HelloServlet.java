3d34719790691e82cc309570f61ff95129a89d3d
AlekseySergeev <Lexxx_95@bk.ru>
2021-02-26 09:59:23 +0300

    new Module TomCat

---
+++ src/main/java/JavaEE/HelloServlet.java
@@ -0,0 +1,23 @@
+package JavaEE;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet {// extends HttpServlet {
+
+//jar -cfv myWar.war *   is command for cmd for create war file.
+    //javac -cp .;"C:\Program Files\Apache Software Foundation\Tomcat 8.5\lib\servlet-api.jar" HelloServlet.java
+    //compile java class in cmd.
+/*
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("hello world");
+    }
+*/
+
+}
